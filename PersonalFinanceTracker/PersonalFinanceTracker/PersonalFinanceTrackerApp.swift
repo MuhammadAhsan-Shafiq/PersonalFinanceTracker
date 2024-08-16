e4f4dce5f479edd8baef7f3a2e5b156b26f8515a
Muhammad Ahsan <ma5639176@gmail.com>
2024-08-16 16:53:00 +0500

    Initial commit - Created Xcode project

---
+++ PersonalFinanceTracker/PersonalFinanceTracker/PersonalFinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalFinanceTrackerApp.swift
+//  PersonalFinanceTracker
+//
+//  Created by MacBook Pro on 16/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct PersonalFinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
